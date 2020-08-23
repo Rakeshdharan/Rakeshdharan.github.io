49639ec98a94c0f5a673ccd848c6ccfa399f87a0
Rakeshdharan <rakesh.18mc@kct.ac.in>
2020-08-23 16:39:55 +0530

    second commmit

--- index_files/PinnedLayer.js
+++
@@ -1,2 +0,0 @@
-(("undefined"!=typeof self?self:this).webpackJsonp_thunderbolt_elements=("undefined"!=typeof self?self:this).webpackJsonp_thunderbolt_elements||[]).push([[176],{1205:function(e,t,n){e.exports={root:"_1w0tU"}},398:function(e,t,n){"use strict";n.r(t);var s=n(135),o=n(1205),r=n.n(o);t.default=function(e){var t=e.id,n=e.children;return s.createElement("div",{id:t,className:r.a.root},n())}}}]);
-//# sourceMappingURL=https://static.parastorage.com/services/editor-elements/dist/PinnedLayer.b0562dd2.chunk.min.js.map
